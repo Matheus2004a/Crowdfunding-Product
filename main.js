9c2011cc783f67923295cd1ac56e2ecc5f329367
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-01-31 21:52:31 -0300

    Adicionando barra de rolagem para os modais de seleção de preços

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const buttonTopPage = document.querySelector(".button-top-page")
+const buttonBookMark = document.querySelector(".container-btn-bookmark")
 
 window.addEventListener("scroll", () => {
     scrollY >= 700 ? buttonTopPage.classList.add("show-button") : buttonTopPage.classList.remove("show-button")
 })
+
+buttonBookMark.addEventListener("click", checkButton)
+
+function checkButton() {
+    let imageBookMark = document.querySelector(".circle-btn-bookmark")
+    let span = document.querySelector(".container-btn-bookmark span")
+
+    imageBookMark.classList.toggle("marked")
+    span.classList.toggle("marked")
+}
