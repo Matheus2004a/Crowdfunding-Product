e240f231b741043a2505cac3a0dcf76362fb875a
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-02-01 18:11:29 -0300

    Seleção de planos corrigida

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ window.addEventListener("scroll", () => {
     scrollY >= 700 ? buttonTopPage.classList.add("show-button") : buttonTopPage.classList.remove("show-button")
 })
 
-buttonBookMark.addEventListener("click", checkButton)
+buttonBookMark.addEventListener("click", checkButtonBookmark)
 
-function checkButton() {
+function checkButtonBookmark() {
     let imageBookMark = document.querySelector(".circle-btn-bookmark")
     let span = document.querySelector(".container-btn-bookmark span")
 
     imageBookMark.classList.toggle("marked")
     span.classList.toggle("marked")
+
+    span.classList.contains("marked") ? span.innerHTML = "Bookmarked" : span.innerHTML = "Bookmark"
 }
