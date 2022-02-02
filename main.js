5b8a313c61c5879aced1bccc3c65502cb1857c12
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-02-02 01:38:36 -0300

    Adicionando campo para entrada de preço ao escolher o plano

--- main.js
+++ main.js
@@ -3,17 +3,33 @@ const buttonBookMark = document.querySelector(".container-btn-bookmark")
 const inputsTrial = document.querySelectorAll(".modal input")
 const articlesTrial = document.querySelectorAll(".articles-modal")
 
-inputsTrial.forEach((input, indexInput) => {
+inputsTrial.forEach(input => {
     input.addEventListener("click", () => {
-        articlesTrial.forEach((article, indexArticle) => {
+        articlesTrial.forEach(article => {
             if (input.checked && article.getElementsByTagName("input")[0] === input) {
                 article.classList.add("check")
+
+                let contentPrice = document.createElement("div")
+                contentPrice.classList.add("content")
+                contentPrice.innerHTML = "<span>Enter your pledge</span> <div><input type='number' placeholder='$ 0.00'> <button type='submit' data-bs-toggle='modal' data-bs-target='#modal-support'>Continue</button></div>"
+
+                article.appendChild(contentPrice)
+
+                /* checkMoreContentAdd()
+
+                function checkMoreContentAdd() {
+                    if (article.classList.contains("content")) {
+                        document.querySelector(".content").style.display = "none"
+                    }
+                } */
+
             } else {
                 article.classList.remove("check")
             }
         })
     })
 })
+
 
 window.addEventListener("scroll", () => {
     scrollY >= 400 ? buttonTopPage.classList.add("show-button") : buttonTopPage.classList.remove("show-button")
